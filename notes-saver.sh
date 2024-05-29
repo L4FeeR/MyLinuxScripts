5a593918b468b2af83b2c609e4205cb8f7dfd798
Muhamed Lafeer <92130586+L4FeeR@users.noreply.github.com>
2024-05-29 23:37:49 +0530

    initial commit on this repo

---
+++ notes-saver.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+ti=`date`
+echo "[$ti]" >> notes.txt
+echo "" >> notes.txt
+echo "$*" >> notes.txt
+echo "" >> notes.txt
+echo "----------------------------------------------------------------------------------" >> notes.txt
+echo "" >> notes.txt
+
+                
