b1530653ab8a3d87d35486a08d3bb5f033a9ee47
L4FeeR <muhamedlafeer837@gmail.com>
2024-08-02 21:41:59 +0530

    ssh-user script...

--- README.m
+++ README.m
@@ -4,4 +4,5 @@ My Own Linux Scripts
 1. Notes-saver.sh   : Save input text into a file with date and time.
 2. safe-file-del.py : Delete files in current dir with shred, for security level integrity
 3. init_build.sh    : Build Linux kernel with deafult config, Only work if run inside Kernel source
+4. ssh-user.sh      : Check users who were online in ssh.<personal script>
 
